bde31618d2a0296b15b710a38818cf707acf9a6c
RitoruX <ppittayut.ritoru@gmail.com>
2023-04-23 00:18:52 +0700

    Add datastore fetching.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { Amplify, DataStore } from 'aws-amplify';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { Task } from './models';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Card } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
@@ -12,25 +11,34 @@ Amplify.configure(awsExports);
 export default function App() {
   return (
     <Authenticator>
-      <IonPage>
-        {({ signOut, user }) => (
-          <main>
-            <h1>Hello {user.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </IonPage>
+      {({ signOut, user }) => (
+        <main>
+          <ListTask></ListTask>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
     </Authenticator>
   );
 }
 
 function ListTask() {
+  const [ data, setData ] = useState([])
   useEffect(() => {
     const loadData = async () => {
-      const posts = await DataStore.query()
+      const tasks = await DataStore.query(Task)
+      setData(tasks)
     }
+
+    loadData()
+
   })
   return (
-    <></>
+    <>
+      { data.map((t) => 
+      <Card>
+        {JSON.stringify(t)}
+      </Card>) }
+    </>
   )
 }
